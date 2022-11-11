ae3b2aaf56c76a30f408709d095f1068f1f386a2
Nvenkat22 <114039086+Nvenkat22@users.noreply.github.com>
2022-11-11 11:09:50 +0530

    Create First.java

---
+++ First.java
@@ -0,0 +1,31 @@
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*int no = 1; 
+		while(no<=10)
+		{
+		System.out.print(no+" "); 
+		no = no+2; 
+	
+	}
+Output:
+      1 3 5 7 9
+
+                int no1 = 0; 
+		while(no1<=8)
+		{
+		System.out.print(no1+" "); 
+		no1 = no1+2; 
+		}
+Output:
+      0 2 4 6 8
+         
+              int no2 = 3; 
+		while(no2<=15)
+		{
+		System.out.print(no2+" "); 
+		no2 = no2+3; 
+		}
+Output:
+      3 6 9 12 15
